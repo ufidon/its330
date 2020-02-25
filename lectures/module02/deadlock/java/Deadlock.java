f7474b2de09a2a86aa62636db7f95e7cd92ba96f
ufidon <ufidon@gmail.com>
2020-02-24 22:32:32 -0600

    lab06 updated

---
+++ lectures/module02/deadlock/java/Deadlock.java
@@ -0,0 +1,43 @@
+// ref: 
+// 1. https://docs.oracle.com/javase/tutorial/essential/concurrency/deadlock.html
+// 2. https://docs.oracle.com/javase/tutorial/essential/concurrency/syncmeth.html
+/** How to compile and run?
+ * Compile: javac Deadlock.java
+ * Run: java Deadlock
+ */
+
+public class Deadlock {
+    static class Friend {
+        private final String name;
+        public Friend(String name) {
+            this.name = name;
+        }
+        public String getName() {
+            return this.name;
+        }
+        public synchronized void bow(Friend bower) {
+            System.out.format("%s: %s"
+                + "  has bowed to me!%n", 
+                this.name, bower.getName());
+            bower.bowBack(this);
+        }
+        public synchronized void bowBack(Friend bower) {
+            System.out.format("%s: %s"
+                + " has bowed back to me!%n",
+                this.name, bower.getName());
+        }
+    }
+
+    public static void main(String[] args) {
+        final Friend alphonse =
+            new Friend("Alphonse");
+        final Friend gaston =
+            new Friend("Gaston");
+        new Thread(new Runnable() {
+            public void run() { alphonse.bow(gaston); }
+        }).start();
+        new Thread(new Runnable() {
+            public void run() { gaston.bow(alphonse); }
+        }).start();
+    }
+}
---
+++ lectures/module02/deadlock/java/DiningPhilosopherswDeadLock.java
@@ -0,0 +1,67 @@
+/**
+ * Ref: 1. https://www.baeldung.com/java-dining-philoshophers 2.
+ * https://github.com/eugenp/tutorials/tree/master/core-java-modules/core-java-concurrency-advanced/src/main/java/com/baeldung/concurrent/diningphilosophers
+ * How to compile & run?
+ * Compile: javac DiningPhilosopherswDeadLock
+ * Run: java DiningPhilosopherswDeadLock
+ */
+
+class Philosopher implements Runnable {
+
+    private final Object leftFork;
+    private final Object rightFork;
+
+    Philosopher(Object left, Object right) {
+        this.leftFork = left;
+        this.rightFork = right;
+    }
+
+    private void doAction(String action) throws InterruptedException {
+        System.out.println(Thread.currentThread().getName() + " " + action);
+        Thread.sleep(((int) (Math.random() * 100)));
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                doAction(System.nanoTime() + ": Thinking"); // thinking
+                synchronized (leftFork) {
+                    doAction(System.nanoTime() + ": Picked up left fork");
+                    synchronized (rightFork) {
+                        doAction(System.nanoTime() + ": Picked up right fork - eating"); // eating
+                        doAction(System.nanoTime() + ": Put down right fork");
+                    }
+                    doAction(System.nanoTime() + ": Put down left fork. Returning to thinking");
+                }
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
+
+
+public class DiningPhilosopherswDeadLock {
+ 
+    public static void main(String[] args) throws Exception {
+ 
+        Philosopher[] philosophers = new Philosopher[5];
+        Object[] forks = new Object[philosophers.length];
+ 
+        for (int i = 0; i < forks.length; i++) {
+            forks[i] = new Object();
+        }
+ 
+        for (int i = 0; i < philosophers.length; i++) {
+            Object leftFork = forks[i];
+            Object rightFork = forks[(i + 1) % forks.length];
+ 
+            philosophers[i] = new Philosopher(leftFork, rightFork);
+             
+            Thread t 
+              = new Thread(philosophers[i], "Philosopher " + (i + 1));
+            t.start();
+        }
+    }
+}
